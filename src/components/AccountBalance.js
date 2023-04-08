6e18eb2f6d7df0ae30ec5bc48fd061e361cef767
Kyle Grande <kyle.g.grande@gmail.com>
2023-04-08 17:39:10 -0500

    Cleaned code

--- src/components/AccountBalance.js
+++ src/components/AccountBalance.js
@@ -4,13 +4,14 @@ src/components/AccountBalance.js
 The AccountBalance component displays account balance. It is included in other page views.
 ==================================================*/
 import React, {Component} from 'react';
+//round accoubt balance to 2 decimal places
 
 class AccountBalance extends Component {
   // Display account balance
   render() {
     return (
       <div>
-        Balance: {this.props.accountBalance}
+        Balance: {this.props.accountBalance.toFixed(2)}
       </div>
     );
   }
--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -4,21 +4,22 @@ src/components/UserProfile.js
 The UserProfile component is used to demonstrate the use of Route and Link.
 Note: You don't need to work on this file for the Assignment.
 ==================================================*/
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-import './UserProfile.css';
+import './UserProfile.css'; // Import CSS styles
 
 class UserProfile extends Component {
   render() {
+    // Render user profile information
     return (
-      <div className='userprofile'>
-        <div className='profile'>
-        <h1>User Profile</h1>
+      <div className="userprofile">
+        <div className="profile">
+          <h1>User Profile</h1>
 
-        <h2>Username: {this.props.userName}</h2>
-        <h3>Member Since: {this.props.memberSince}</h3>
-        <br/>
-      </div>
+          <h2>Username: {this.props.userName}</h2>
+          <h3>Member Since: {this.props.memberSince}</h3>
+          <br />
+        </div>
       </div>
     );
   }
